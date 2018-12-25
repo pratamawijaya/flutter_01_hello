3fe940666fe537adca30b4ec79f80e52805cd526
pratamawijaya <set.mnemonix@gmail.com>
2018-12-25 14:03:01 +0700

    change theme of material app

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
+      theme: new ThemeData(primaryColor: Colors.teal),
     );
   }
 }
